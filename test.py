c0a761f114420a9098786f9b3b7b25873d1e5316
danieltiandev <danieltiandev@gmail.com>
2026-01-28 22:02:52 -0800

    array learn

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-
+import array
 
 
 def addTwoValues(a, b):
@@ -10,8 +10,31 @@ def addThreeValues(a,b,c):
     return finalResult
 
 
-sum = addTwoValues(5,3)
-print("Sum of two values:", sum)
+def modifyArray(arr):
+    for i in range(len(arr)):
+        arr[i] = arr[i] * 2
+    return arr
 
-sum2 = addThreeValues(2,4,6)
-print("Sum of three values:", sum2)
+def arraySum(arr):
+    total = 0
+    for i in range(len(arr)):
+        total += arr[i]
+    return total
+
+
+
+my_list = [1, 2, 3, 4, 5]
+
+my_array = array.array('i', [1, 2, 3, 4, 10])
+
+sum1 = arraySum(my_array)
+sum2 = sum(my_list)
+print("Sum of array elements:", sum1)
+print("Sum of list elements:", sum2)
+
+
+# sum = addTwoValues(5,3)
+# print("Sum of two values:", sum)
+
+# sum2 = addThreeValues(2,4,6)
+# print("Sum of three values:", sum2)
